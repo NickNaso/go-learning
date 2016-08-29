492d96a9b231ab8475ef29fe960449a9ac5b1f4d
NickNaso <nicoladelgobbo@gmail.com>
2016-08-29 23:59:56 +0200

    Update .gitignore Add  First example

---
+++ ch01/echo1.go
@@ -0,0 +1,15 @@
+package ch01
+
+import (
+    "fmt"
+    "os"
+)
+
+func main ()  {
+	var s, sep string
+	for i:= 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s);
+}
